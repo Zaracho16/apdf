07e4fddb493c3ed6b06b5cb67625111979de4d26
Zaracho16 <adrianzaracho16@gmail.com>
2026-09-15 22:37:32 -0300

    feat: muestra imagenes seleccionadas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,34 +1,67 @@
 
 import * as ImagePicker from 'expo-image-picker';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 export default function HomeScreen() {
+  const [imagenes, setImagenes] = useState<string[]>([]);
+
+  const seleccionarImagenes = async () => {
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsMultipleSelection: true,
+    });
+
+    if (!resultado.canceled) {
+      const nuevasImagenes = resultado.assets.map(
+        (imagen) => imagen.uri
+      );
+
+      setImagenes(nuevasImagenes);
+    }
+  };
 
   return (
-
     <View style={styles.container}>
 
-      <Text style={styles.title}> APDF </Text>
-
-      <Text style={styles.subtitle}> 
-        Convierte tus imagenes en PDF 
+      <Text style={styles.title}>
+        APDF
       </Text>
 
-      <Pressable style={styles.button} onPress={seleccionarImagenes}>
+      <Text style={styles.subtitle}>
+        Convierte tus imagenes en PDF
+      </Text>
+
+      <Pressable
+        style={styles.button}
+        onPress={seleccionarImagenes}
+      >
         <Text style={styles.buttonText}>
           Seleccionar imagenes
         </Text>
       </Pressable>
 
+      <View style={styles.imageContainer}>
+        {imagenes.map((imagen, index) => (
+          <Image
+            key={index}
+            source={{ uri: imagen }}
+            style={styles.image}
+          />
+        ))}
+      </View>
+
     </View>
-
   );
-
 }
 
-
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -59,14 +92,17 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 
+  imageContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    gap: 10,
+    marginTop: 20,
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
 });
-
-/* Funcion para abrir la galeria */
-const seleccionarImagenes = async() => {
-  const resultado = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ['images'],
-    allowsMultipleSelection: true,
-  });
-
-  console.log(resultado);
-};
